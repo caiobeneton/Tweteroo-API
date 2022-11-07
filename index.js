493108aa472d5a196922597cbff63b4d3fba1862
Caio Beneton <caio.beneton@gmail.com>
2022-11-07 13:48:38 -0300

    finalização

--- index.js
+++ index.js
@@ -1,57 +1,57 @@
 import express, { json } from "express"
 import cors from "cors"
 
-const app = express()
-app.use(cors())
-app.use(json())
+const app = express();
+app.use(cors());
+app.use(json());
 
-const users = []
-const tweets = []
+const users = [];
+const tweets = [];
 
 app.post("/sign-up", (req, res) => {
-    const {username, avatar} = req.body
+    const {username, avatar} = req.body;
 
     if (!username || !avatar) {
-        res.status(422).send("Todos os campos são obrigatórios")
+        res.status(422).send("Todos os campos são obrigatórios");
         return
-    }
+    };
 
     const user = {
         username,
         avatar
-    }
+    };
 
-    users.push(user)
-    res.send("OK")
-})
+    users.push(user);
+    res.send("OK");
+});
 
 app.post("/tweets", (req, res) => {
-    const {username, tweet} = req.body
+    const {username, tweet} = req.body;
 
-    const userData = users.find((obj) => obj.username === username)
+    const userData = users.find((obj) => obj.username === username);
 
     const objTweet = {
         username,
         avatar: userData.avatar,
         tweet
-    }
+    };
 
-    tweets.push(objTweet)
-    res.send("OK")
-})
+    tweets.push(objTweet);
+    res.send("OK");
+});
 
 app.get("/tweets", (req, res) => {
     
     if (tweets.length <= 10) {
-        const reverse = [...tweets].reverse()
-        res.send(reverse)
+        const reverse = [...tweets].reverse();
+        res.send(reverse);
     } else {
         const recent = tweets.slice(-10)
-        const reverse = [...recent].reverse()
-        res.send(reverse)
+        const reverse = [...recent].reverse();
+        res.send(reverse);
     }
-})
+});
 
 app.listen(5000, () => {
-    console.log("server running on port 5000")
-})
+    console.log("server running on port 5000");
+});
